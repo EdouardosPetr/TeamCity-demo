094428d36ffbfe1c20d8a0422cb34144178859c2
EdouardosPetr <42935323+EdouardosPetr@users.noreply.github.com>
2024-11-25 17:29:20 +0100

    Create Project.kt

---
+++ .teamcity/Project.kt
@@ -0,0 +1,30 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+version = "2024.1"
+
+project {
+    name = "Hello World Project"
+
+    buildType(HelloWorldBuild)
+}
+
+object HelloWorldBuild : BuildType({
+    name = "Build Hello World"
+
+    steps {
+        script {
+            name = "Run Hello World"
+            scriptContent = "node index.js"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+})
