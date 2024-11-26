4241d049adcc53fcd7a4bab9750d3bc1cf569d94
EdouardosPetr <42935323+EdouardosPetr@users.noreply.github.com>
2024-11-26 15:31:28 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,33 +1,14 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.v2020_2.*
+import jetbrains.buildServer.configs.kotlin.v2024_1.*
 
 version = "2024.1"
 
 project {
     name = "Hello World Project"
 
-    vcsRoot(DslContext.settingsRoot)  // Link to the VCS
+    // Link the VCS root (GitHub repository)
+    vcsRoot(DslContext.settingsRoot)
 
+    // Register the build configuration
     buildType(BuildHelloWorld)
 }
 
-object BuildHelloWorld : BuildType({
-    name = "Build Hello World"
-
-    steps {
-        script {
-            name = "Check Node Version"
-            scriptContent = "node -v"  // Verifies Node.js is installed
-        }
-        script {
-            name = "Run Hello World"
-            scriptContent = "node index.js"
-        }
-    }
-
-    triggers {
-        vcs {
-            // Trigger builds on changes
-        }
-    }
-})
