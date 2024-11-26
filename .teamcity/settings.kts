f957f2750fa0d5ff4cf11b130f4aaae934aa6aac
EdouardosPetr <42935323+EdouardosPetr@users.noreply.github.com>
2024-11-26 15:22:03 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,33 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2020_2.*
 
 version = "2024.1"
 
-// This file is where you can define your TeamCity project settings.
 project {
     name = "Hello World Project"
-    // Add more settings here if necessary.
+
+    vcsRoot(DslContext.settingsRoot)  // Link to the VCS
+
+    buildType(BuildHelloWorld)
 }
+
+object BuildHelloWorld : BuildType({
+    name = "Build Hello World"
+
+    steps {
+        script {
+            name = "Check Node Version"
+            scriptContent = "node -v"  // Verifies Node.js is installed
+        }
+        script {
+            name = "Run Hello World"
+            scriptContent = "node index.js"
+        }
+    }
+
+    triggers {
+        vcs {
+            // Trigger builds on changes
+        }
+    }
+})
