a6a4ff73f6cc210be347c828c5a6fba1a371167a
EdouardosPetr <42935323+EdouardosPetr@users.noreply.github.com>
2024-11-26 14:26:52 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,35 +1,10 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.*
-import jetbrains.buildServer.configs.kotlin.triggers.*
-import jetbrains.buildServer.configs.kotlin.features.*
-
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2024.03"
 
 project {
 
+    // Define a simple build type
     buildType(Build)
 }
 
@@ -37,36 +12,13 @@ object Build : BuildType({
     name = "Build"
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(DslContext.settingsRoot)  // Use the root VCS defined for the project
     }
 
     steps {
-        nodeJS {
-            id = "nodejs_runner"
-            shellScript = "npm install"
-        }
-        nodeJS {
-            id = "nodejs_runner_1"
-            shellScript = "npm run test"
-        }
         script {
-            name = "List Workspace"
-            id = "List_Workspace"
-            scriptContent = "ls -R /mnt/agent/work/"
-        }
-        kotlinFile {
-            id = "kotlinScript"
-            path = "settings.kts"
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
+            name = "Simple Script"
+            scriptContent = "echo 'Hello, TeamCity!'"  // Just a simple echo command for debugging
         }
     }
 })
